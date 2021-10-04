01a09f5f18a56b37315efbf408d73920ddcd1a06
Pascal Rouppert <pascal.rouppert@telecom-paris.fr>
2021-10-04 14:45:25 +0200

    Fonction fibo

--- TD_bare_metal/main.c
+++ TD_bare_metal/main.c
@@ -1,6 +1,17 @@
 #include <stdlib.h>
 
+int fibo(int n);
+
 int main() {
-    while (1) {};
+    return fibo(8);
 }
 
+int fibo(int n) {
+    if (n==0 || n==1) {
+        return(1);
+    }
+
+    else {
+        return (fibo(n-1) + fibo(n-2));
+    }
+}
