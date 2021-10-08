8f756dbc65c9a1a7c4523766e71fd277320f3866
Pascal Rouppert <pascal.rouppert@telecom-paris.fr>
2021-10-08 10:30:59 +0200

    memfunc

---
+++ TD_bare_metal/memfunc.c
@@ -0,0 +1,57 @@
+#include <stddef.h>
+#include <stdint.h>
+
+void *memset(void *s, int c, size_t n);
+void *memcpy(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+
+void *memset(void *s, int c, size_t n) {
+    uint8_t* i = s;
+    while ((int)i < n) {
+        *i = c;
+        i++;
+    } return s;
+}
+
+void *memcpy(void *dest, const void *src, size_t n) {
+    if (dest + n >= src) {
+        return 0;
+    }
+    uint8_t i = 0;
+    while (i < n * sizeof(uint8_t)) {
+        *((uint8_t*)dest + i) = *((uint8_t*)src + i);
+        i += sizeof(uint8_t);
+    } return dest;
+}
+
+void *memmove(void *dest, const void *src, size_t n) {
+    if (dest < src) {
+        int diff = src - dest;
+        for (uint8_t i = 0; i < diff || i < n; i++) {
+            *((uint8_t*)dest + i)= *((uint8_t*)src + i);
+        }
+    }
+    else {
+        int diff = dest - src;
+        int min;
+        if (diff > n) {
+            int min = n;
+        }
+        else {
+            int min = diff;
+        }
+        for (uint8_t i = min; i > 0; i--) {
+            *((uint8_t*)dest + i)= *((uint8_t*)src + i);
+        }
+    }
+}
+
+int memcmp(const void *s1, const void *s2, size_t n) {
+    for (uint8_t i = 0; i < n; i++) {
+        uint8_t diff = ((uint8_t*)s2 + i) - ((uint8_t*)s1 + i);
+        if (diff != 0) {
+            return diff;
+        }
+    } return 0;
+}
