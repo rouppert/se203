113c68e56f68f6de5d4f2554675bbdde0c16c886
Pascal Rouppert <pascal.rouppert@telecom-paris.fr>
2021-10-11 13:57:08 +0200

    clignotement de la led

--- TD_bare_metal/main.c
+++ TD_bare_metal/main.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
+#include "led.h"
+
 
 int main() {
     led_init();
-    led_g_on();
-    led_g_off();
+    int i = 0;
+    while (i++ < 15) {
+        led_g_on();
+        for (int i=0; i<15; i++) asm volatile("nop");
+        led_g_off();
+    }      
 }
