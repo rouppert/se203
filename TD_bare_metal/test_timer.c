28d166e4707c5948e40d86d2ea32ad13677535b3
Pascal Rouppert <pascal.rouppert@telecom-paris.fr>
2021-11-11 21:00:42 +0100

    fonctions pur l'affichage à 60Hz

--- TD_bare_metal/test_timer.c
+++ TD_bare_metal/test_timer.c
@@ -2,11 +2,20 @@
 #include "clocks.h"
 #include "led.h"
 #include "irq.h"
+#include "display_frame.h"
 
+//Fonction main pour la premiere partie du timer : fait clignoter la LED une fois par seconde.
+/*
 int main() {
     clocks_init();
     irq_init();
     led_init();
-    timer_init(1000);
+    timer_init(1000000);
     while (1) {};
+}
+*/
+
+//Fonction main pour la deuxième partie du timer : affiche l'image envoyée à la fréquence de 60Hz.
+int main() {
+    display_file_60();
 }
